b197b6f74ebf98237b3b53b2a589a09ca44c8834
Marco <narakucoronel@gmail.com>
2024-09-13 02:54:19 -0400

    Feature (common): Base project added

---
+++ react-example/src/components/Button.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export const Button=props=>{
+    const{text}=props;  //'text' is MimeTypeArray
+
+    const[count, setCount ] = useState (0);
+    function handlerButton(){
+        setCount(count+1);
+    }
+    return(
+        <button onClick={handlerButton}>
+            {text}{count}
+        </button>
+    );
+}
---
+++ react-example/src/components/WrapperList.jsx
@@ -0,0 +1,14 @@
+
+  export const WapperList = props => {
+  
+    const { children } = props
+  
+    return (
+      <div>
+        <h2>Lista de Compras</h2>
+        <div>
+          { children }
+        </div>
+      </div>
+    )
+  }
